7d7c58f39c92d72066bd8af76475e537738e15e9
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-07-31 00:33:42 -0500

    Added interaction logic with UI

---
+++ Source/Game/Interactions/AElevatorButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using DigiTalino_Plugin;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// AElevatorButton Script.
+/// </summary>
+public class AElevatorButton : AInteraction
+{
+
+    protected override void OnInteract(Actor interactor)
+    {
+
+    }
+
+}
---
+++ Source/Game/UI/InteractionUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DigiTalino_Plugin;
+
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace Game;
+
+/// <summary>
+/// InteractionUI Script.
+/// </summary>
+public class InteractionUI : Script
+{
+    [ShowInEditor, Serialize] private UIControl _contentControl;
+    [ShowInEditor, Serialize] private UIControl _interactionTextControl;
+
+    private Label _interactionText;
+
+
+    public override void OnAwake()
+    {
+        _interactionText = DigiTalino_Plugin.UIHelper.GetLabel(_interactionTextControl);
+        _contentControl.Control.Visible = false;
+    }
+
+    public override void OnStart()
+    {
+        SingletonManager.Get<InteractionComponent>().OnFocusChanged += OnFocusChanged;
+    }
+
+    private void OnFocusChanged(string obj)
+    {
+        _contentControl.Control.Visible = !string.IsNullOrEmpty(obj);
+        _interactionText.Text = obj;
+    }
+
+}
