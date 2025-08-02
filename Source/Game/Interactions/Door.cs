27bd79211b24d9ffe3694c33af979e9ccd70982b
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-08-01 21:57:41 -0500

    Added Door script and refactored plugin to use script

---
+++ Source/Game/Interactions/Door.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Door Actor.
+/// </summary>
+public class Door : Script
+{
+    /// <inheritdoc/>
+    public override void OnAwake()
+    {
+        base.OnAwake();
+
+    }
+
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        base.OnStart();
+
+    }
+
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+    }
+
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+    }
+
+
+}
