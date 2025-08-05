f3589bc9742f97e341568d04204acc29df56c79b
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-08-05 13:30:45 -0500

    Create LightSwitch.cs

---
+++ Source/Game/Interactions/LightSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using DigiTalino_Plugin;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// LightSwitch Actor.
+/// </summary>
+[Category("Interactions")]
+public class LightSwitch : AInteraction
+{
+    [ShowInEditor, Serialize] private Actor _LightActor;
+    private bool _IsOn = true;
+
+
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+        ToggleLight();
+        _isToggleable = true;
+    }
+
+    /// <inheritdoc/>
+    protected override void OnInteract(Actor interactor)
+    {
+        ToggleLight();
+
+    }
+
+
+    private void ToggleLight()
+    {
+        _IsOn = !_IsOn;
+        _LightActor.IsActive = _IsOn;
+        Debug.Log("Toggle Light: " + _IsOn);
+    }
+
+
+}
